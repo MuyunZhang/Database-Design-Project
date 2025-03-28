50aae20643ec6c29b8daab404c3f6052248bcdf3
BT_4N2_16 <muyunz@nycstudents.net>
2025-03-28 14:24:28 -0400

    ds

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] floors = {"B", "1", "2", "3", "4", "5", "6", "7", "8"};
+        String[] wing = {"N", "S", "E", "W"};
+
+        for (int k = 0; k < 4; k ++) {
+            for (int i = 1; i <= 20; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+}
