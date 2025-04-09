b9416ed88eceb8e8c122b35deefdaf9d86f1ddb3
BT_4N2_32 <muyunz@nycstudents.net>
2025-04-09 14:29:43 -0400

    courses

--- src/Assignments.java
+++ src/Assignments.java
@@ -1,3 +1,13 @@
-public class Assignments {
+import java.util.ArrayList;
 
+public class Assignments {
+        public static void main(String[] args) {
+            String [] types = {"Minor Assessment", "Major Assessment"};
+            for (int i = 1; i <= types.length; i++) {
+                System.out.println("INSERT INTO Assignment_Type ( Assignment_TypeID, Assignment_Type ) VALUES ( " + i + ", ' " + types[i-1] + "' );");
+            }
+            for (int i = 1; i <= types.length; i++) {
+                System.out.println("INSERT INTO Assignments ( Assignment_ID, Assignment_Name, Class_ID, Assignment_TypeID) VALUES ( " + i + ", ' " + types[i-1] + "' );");
+            }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,9 +7,11 @@ public class Main {
         String[] floors = {"B", "1", "2", "3", "4", "5", "6", "7", "8"};
         String[] wing = {"N", "S", "E", "W"};
         //rooms
+        ArrayList<Integer> rooms = new ArrayList<>();
         for (int k = 0; k < 9; k ++) {
             for (int w = 0; w < 4; w ++) {
                 for (int i = 1; i <= 20; i++) {
+                    rooms.add(i);
                     System.out.println(floors[k] + wing[w] + i);
                 }
             }
