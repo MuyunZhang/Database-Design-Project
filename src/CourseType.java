a1096d760edb3f42b951536803f50f9383a77e4f
BT_4N2_32 <muyunz@nycstudents.net>
2025-04-22 14:36:30 -0400

    courses

--- src/CourseType.java
+++ src/CourseType.java
@@ -5,5 +5,27 @@ public class CourseType {
             System.out.println("INSERT INTO Course_Type ( Course_TypeID, Course_Type ) VALUES ( " + i + ", ' " + types[i-1] + "' );");
         }
     }
+    private String courseType;
+    private int courseTypeID;
 
+    public CourseType(int id, String type){
+        courseType = type;
+        courseTypeID = id;
+    }
+
+    public void setCourseType(String courseType) {
+        this.courseType = courseType;
+    }
+
+    public void setCourseTypeID(int courseTypeID) {
+        this.courseTypeID = courseTypeID;
+    }
+
+    public String getCourseType() {
+        return courseType;
+    }
+
+    public int getCourseTypeID() {
+        return courseTypeID;
+    }
 }
